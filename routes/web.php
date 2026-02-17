87bf138b1df601fb8c0474a1a32cfec33b14d0ed
Navdeep Pathan <151203532+navdeeppathan@users.noreply.github.com>
2026-02-17 08:24:26 +0530

    logo fix

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('home');
+});
+
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('register.store');
 
